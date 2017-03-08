9f36241c37c05581b769de30776aea7a2925f431
Mikko Harju <mikko@taiste.fi>
2017-03-08 18:39:24 +0200

    Added WebAudio API version

--- src/soundboard/core.cljs
+++ src/soundboard/core.cljs
@@ -1,31 +1,51 @@
 (ns soundboard.core
-    (:require [reagent.core :as reagent]))
+    (:require [reagent.core :as reagent]
+              [goog.events :as ev]))
+
+(defonce app-state (atom {}))
+
+;; Web Audio API
+(defn get-context [] (:context @app-state))
+
+(defn sound-name [identifier] (str "/sounds/" (if (keyword? identifier) (name identifier) identifier) ".wav"))
+
+(defn sound-loaded [req sound]
+  (fn [response]
+    (let [context (get-context)]
+        (.decodeAudioData context (.-response req)
+                          (fn [data]
+                            (swap! app-state assoc sound data))))))
+
+(defn load-sound [sound]
+  (let [req (js/XMLHttpRequest.)]
+    (.open req "GET" (sound-name sound) true)
+    (set! (.-responseType req) "arraybuffer")
+    (set! (.-onload req) (sound-loaded req sound))
+    (.send req)))
+
+
+(defn play-sound [sound]
+  (let [source (.createBufferSource (get-context))]
+    (set! (.-buffer source) (get @app-state sound))
+    (.connect source (.-destination (get-context)))
+    (.start source 0)))
+
+(defn init-context! [] (when (nil? (:context @app-state)) (swap! app-state assoc :context (js/AudioContext.))))
 
 ;; -------------------------
 ;; Views
 
-(def sounds [:faster :harder :stronger :kick :sd :hihat])
-
 (defn audio-button [sound]
   (let [identifier (name sound)]
-    [:div.audio-button
-     [:audio {:id identifier}
-       [:source {:src (str "sounds/" identifier ".wav")}]]
-     [:button
-       {:on-click
-         (fn [e]
-           (.play
-             (.getElementById js/document identifier)))}
-       identifier]]))
+      [:div.audio-button
+        [:button {:on-click #(play-sound sound)} identifier]]))
+
+(def sounds [:faster :harder :kick :sd :hihat])
 
 (defn home-page []
   [:div
-    (map (fn [sound]
-           ^{:key sound}
-           [audio-button sound])
-         sounds)])
-
-;; <div><h2>Terve vaan Asteriski ja Digit!</h2></div>
+    [:h1 "TAISTE SOUNDBOARD"]
+    (into [:div.sounds] (map (fn [sound] ^{:key sound} [audio-button sound]) sounds))])
 
 ;; -------------------------
 ;; Initialize app
